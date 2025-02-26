2c5910b2510d5ad8494b2b09817bba84283c8061
Robert Vrooman <160907456+robert-at-pieces@users.noreply.github.com>
2025-02-26 12:29:52 -0500

    Update curl.rb
    Removing tty requirement for not giving '--silent' flag to curl.

--- Library/Homebrew/utils/curl.rb
+++ Library/Homebrew/utils/curl.rb
@@ -130,7 +130,7 @@ module Utils
         args << "--fail"
         args << "--progress-bar" unless Context.current.verbose?
         args << "--verbose" if Homebrew::EnvConfig.curl_verbose?
-        args << "--silent" if !$stdout.tty? || Context.current.quiet?
+        args << "--silent" if Context.current.quiet?
       end
 
       args << "--connect-timeout" << connect_timeout.round(3) if connect_timeout.present?
